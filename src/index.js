b733b47a658736487fe7fa31c9d29baa84220c0a
reactvirani <tulsivirani88@gmail.com>
2022-05-03 10:00:13 +0530

    2 may

--- src/index.js
+++ src/index.js
@@ -11,6 +11,22 @@ root.render(
   </React.StrictMode>
 );
 
+const Users = () => {
+  const data = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Victor Wayne" },
+    { id: 3, name: "Jane Doe" },
+  ];
+
+  return (
+    <div className="users">
+      {data.map((user) => (
+        <div className="user">{user}</div>
+      ))}
+    </div>
+  );
+};
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
